3d2c2f0721474ffee78ace1130565fbf78bc8a24
Nation Chirara <Nation.Chirara@cassavasmartech.co.zw>
2020-07-05 13:52:07 +0200

    Project Init + Product List Page:Nice

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'amc-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'amc-shop';
+}
