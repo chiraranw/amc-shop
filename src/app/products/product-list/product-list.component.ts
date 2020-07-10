2a633f4815b401e04fd9acc4497a65f0bb51910d
Nation Chirara <chiraranw@gmail.com>
2020-07-10 14:07:08 +0200

    data binding and pipes

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -3,13 +3,42 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'amc-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
+  public title: string = 'Product List';
+  public imageWidth: number = 50;
+  public imageMargin: number = 2;
+  public showImage: boolean = false;
+  public productFilter: string = 'cart';
 
-  constructor() { }
+  public products: any[] = [
+    {
+      productId: 1,
+      productName: 'Leaf Rake',
+      productCode: 'GDN-0011',
+      releaseDate: 'March 19, 2019',
+      description: 'Leaf rake with 48-inch wooden handle.',
+      price: 19.95,
+      starRating: 3.2,
+      imageUrl: 'assets/images/leaf_rake.png',
+    },
+    {
+      productId: 2,
+      productName: 'Garden Cart',
+      productCode: 'GDN-0023',
+      releaseDate: 'March 18, 2019',
+      description: '15 gallon capacity rolling garden cart',
+      price: 32.99,
+      starRating: 4.2,
+      imageUrl: 'assets/images/garden_cart.png',
+    },
+  ];
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public toggleImage(): void {
+    this.showImage = !this.showImage;
   }
-
 }
