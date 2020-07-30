6b8d780706aa7f56b62aa84cfaf3d84008d95b3f
Nation Chirara <chiraranw@gmail.com>
2020-07-30 13:08:51 +0200

    Setting up Spinner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,13 @@
 import { Component } from '@angular/core';
 import { AuthService } from './user/auth.service';
-import { Router } from '@angular/router';
+import {
+  Router,
+  Event,
+  NavigationStart,
+  NavigationEnd,
+  NavigationCancel,
+  NavigationError,
+} from '@angular/router';
 
 @Component({
   selector: 'amc-root',
@@ -9,6 +16,27 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
   pageTitle = 'Acme Product Management';
+  public loading: boolean;
+
+  constructor(private authService: AuthService, private router: Router) {
+    this.router.events.subscribe((routerEvent: Event) => {
+      this.checkoutRouterEvents(routerEvent);
+    });
+  }
+
+  private checkoutRouterEvents(routerEvent): void {
+    if (routerEvent instanceof NavigationStart) {
+      this.loading = true;
+    }
+
+    if (
+      routerEvent instanceof NavigationEnd ||
+      routerEvent instanceof NavigationCancel ||
+      routerEvent instanceof NavigationError
+    ) {
+      this.loading = false;
+    }
+  }
 
   get isLoggedIn(): boolean {
     return this.authService.isLoggedIn;
@@ -20,8 +48,6 @@ export class AppComponent {
     }
     return '';
   }
-
-  constructor(private authService: AuthService, private router: Router) {}
 
   logOut(): void {
     this.authService.logout();
